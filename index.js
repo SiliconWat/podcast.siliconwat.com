39703ec9df27b738125051c660b18bd3941337f8
Thon Ly <thonly@heartbank.ceo>
2021-09-14 06:56:40 -0700

    created architecture

---
+++ index.js
@@ -0,0 +1,22 @@
+const winners = {
+    senior: {
+        extreme: {
+            username: "3iitos",
+            profile: "https://twitter.com/3iitos"
+        },
+        hard: {
+            username: "3iitos",
+            profile: "https://twitter.com/3iitos"
+        }
+    },
+    junior: {
+        medium: {
+            username: "panhiatha0",
+            profile: "https://twitter.com/panhiatha0"
+        },
+        easy: {
+            username: "panhiatha0",
+            profile: "https://twitter.com/panhiatha0"
+        }
+    }
+}
---
+++ senior/senior.js
@@ -0,0 +1,16 @@
+const monks = [
+    {
+        username: "3iitos",
+        profile: "https://twitter.com/3iitos",
+        winnings: 3,
+        currentStreak: 2,
+        longestStreak: 5
+    }, 
+    {
+        username: "VitalikButerin",
+        profile: "https://twitter.com/VitalikButerin",
+        winnings: 3,
+        currentStreak: 2,
+        longestStreak: 5
+    }
+]
