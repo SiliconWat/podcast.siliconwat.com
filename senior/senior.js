8f66bde32de8df6376c316cd7e08438df1295887
Thon Ly <thonly@heartbank.ceo>
2021-09-15 07:31:59 -0700

    added js

--- senior/senior.js
+++ senior/senior.js
@@ -9,8 +9,59 @@ const monks = [
     {
         username: "VitalikButerin",
         profile: "https://twitter.com/VitalikButerin",
-        winnings: 3,
-        currentStreak: 2,
-        longestStreak: 5
+        winnings: 2,
+        currentStreak: 1,
+        longestStreak: 3
     }
 ]
+
+function orderByMonks() {
+    monks.sort((a, b) => b.username.localeCompare(a.username))
+    generateScoreboard()
+}
+
+function orderByWinnings() {
+    monks.sort((a, b) => b.winnings - a.winnings)
+    generateScoreboard()
+}
+
+function orderByCurrentStreaks() {
+    monks.sort((a, b) => b.currentStreak - a.currentStreak)
+    generateScoreboard()
+}
+
+function orderByLongestStreaks() {
+    monks.sort((a, b) => b.longestStreak - a.longestStreak)
+    generateScoreboard()
+}
+
+function generateScoreboard () {
+    const tbody = document.querySelector("tbody")
+    tbody.innerHTML = ""
+
+    monks.forEach(monk => {
+        const tr = document.createElement("tr")
+        const a = document.createElement("a")
+        const username = document.createElement("td")
+        const winnings = document.createElement("td")
+        const currentStreak = document.createElement("td")
+        const longestStreak = document.createElement("td")
+
+        a.textContent = monk.username
+        a.href = monk.profile
+        username.appendChild(a)
+
+        winnings.textContent = monk.winnings
+        currentStreak.textContent = monk.currentStreak
+        longestStreak.textContent = monk.longestStreak
+
+        tr.appendChild(username)
+        tr.appendChild(winnings)
+        tr.appendChild(currentStreak)
+        tr.appendChild(longestStreak)
+
+        tbody.appendChild(tr)
+    })
+}
+
+orderByWinnings()
