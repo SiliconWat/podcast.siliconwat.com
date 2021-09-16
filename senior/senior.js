ffa5d35c4e0d78bf543c88ad3b8a32037e4a133a
Thon Ly <thonly@heartbank.ceo>
2021-09-16 08:03:38 -0700

    version 1 complete

--- senior/senior.js
+++ senior/senior.js
@@ -1,67 +1,26 @@
+import {orderByMonks, orderByWinnings, orderByCurrentStreaks, orderByLongestStreaks} from "../library.mjs"
+
 const monks = [
     {
         username: "3iitos",
         profile: "https://twitter.com/3iitos",
-        winnings: 3,
-        currentStreak: 2,
-        longestStreak: 5
+        winnings: 0,
+        currentStreak: 0,
+        longestStreak: 0
     }, 
     {
         username: "VitalikButerin",
         profile: "https://twitter.com/VitalikButerin",
-        winnings: 2,
-        currentStreak: 1,
-        longestStreak: 3
+        winnings: 0,
+        currentStreak: 0,
+        longestStreak: 0
     }
 ]
 
-function orderByMonks() {
-    monks.sort((a, b) => b.username.localeCompare(a.username))
-    generateScoreboard()
-}
+window.monks = monks
+window.orderByMonks = orderByMonks
+window.orderByWinnings = orderByWinnings
+window.orderByCurrentStreaks = orderByCurrentStreaks
+window.orderByLongestStreaks = orderByLongestStreaks
 
-function orderByWinnings() {
-    monks.sort((a, b) => b.winnings - a.winnings)
-    generateScoreboard()
-}
-
-function orderByCurrentStreaks() {
-    monks.sort((a, b) => b.currentStreak - a.currentStreak)
-    generateScoreboard()
-}
-
-function orderByLongestStreaks() {
-    monks.sort((a, b) => b.longestStreak - a.longestStreak)
-    generateScoreboard()
-}
-
-function generateScoreboard () {
-    const tbody = document.querySelector("tbody")
-    tbody.innerHTML = ""
-
-    monks.forEach(monk => {
-        const tr = document.createElement("tr")
-        const a = document.createElement("a")
-        const username = document.createElement("td")
-        const winnings = document.createElement("td")
-        const currentStreak = document.createElement("td")
-        const longestStreak = document.createElement("td")
-
-        a.textContent = monk.username
-        a.href = monk.profile
-        username.appendChild(a)
-
-        winnings.textContent = monk.winnings
-        currentStreak.textContent = monk.currentStreak
-        longestStreak.textContent = monk.longestStreak
-
-        tr.appendChild(username)
-        tr.appendChild(winnings)
-        tr.appendChild(currentStreak)
-        tr.appendChild(longestStreak)
-
-        tbody.appendChild(tr)
-    })
-}
-
-orderByWinnings()
+orderByWinnings(monks)
